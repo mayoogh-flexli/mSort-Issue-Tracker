bd7bf15e040648baa8783db31b9350dd2a301185
mayoogh-flexli <148973130+mayoogh-flexli@users.noreply.github.com>
2025-11-11 18:13:29 +0530

    Delete Bot Health Dashboard directory

--- Bot Health Dashboard/config.js
+++
@@ -1,6 +0,0 @@
-window.MSORT_CONFIG = {
-    REPO_OWNER: "mayoogh-flexli",
-    REPO_NAME: "mSort-Issue-Tracker",
-    AUTO_REFRESH_INTERVAL: 30000, // 30 seconds
-    GITHUB_API_BASE: "https://api.github.com"
-};
